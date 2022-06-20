1ffe483a4b92ad5112a4367f816ed9507e4e654c
Shower-Wine <76917570+Shower-Wine@users.noreply.github.com>
2022-06-20 16:11:37 +0900

    Add files via upload

--- signup.js
+++ signup.js
@@ -1,4 +1,3 @@
-// 휴대폰 번호 입력 부분
 function changePhone1(){
     const phone1 = document.getElementById("phone1").value // 010
     if(phone1.length === 3){
@@ -20,7 +19,6 @@ function changePhone3(){
     }
 }
 
-// 문자인증+타이머 부분
 function initButton(){
   document.getElementById("sendMessage").disabled = true;
   document.getElementById("completion").disabled = true;
@@ -34,7 +32,6 @@ let processID = -1;
 
 const getToken = () => {
 
-  // 인증확인 버튼 활성화
   document.getElementById("completion").setAttribute("style","background-color:yellow;")
   document.getElementById("completion").disabled = false;
 
@@ -65,8 +62,6 @@ function checkCompletion(){
 }
 
 
-// 가입부분 체크
-
 function signUpCheck(){
 
   let email = document.getElementById("email").value
@@ -78,7 +73,6 @@ function signUpCheck(){
   let gender_woman = document.getElementById("gender_woman").checked
   let check = true;
 
-  // 이메일확인
   if(email.includes('@')){
     let emailId = email.split('@')[0]
     let emailServer = email.split('@')[1]
@@ -94,8 +88,6 @@ function signUpCheck(){
     check = false
   }
 
-
-  // 이름확인
   if(name===""){
     document.getElementById("nameError").innerHTML="이름이 올바르지 않습니다."
     check = false
@@ -104,7 +96,6 @@ function signUpCheck(){
   }
 
 
-  // 비밀번호 확인
   if(password !== passwordCheck){
     document.getElementById("passwordError").innerHTML=""
     document.getElementById("passwordCheckError").innerHTML="비밀번호가 동일하지 않습니다."
@@ -118,17 +109,16 @@ function signUpCheck(){
     document.getElementById("passwordError").innerHTML="비밀번호를 입력해주세요."
     check = false
   }else{
-    //document.getElementById("passwordError").innerHTML=""
+
   }
   if(passwordCheck===""){
     document.getElementById("passwordCheckError").innerHTML="비밀번호를 다시 입력해주세요."
     check = false
   }else{
-    //document.getElementById("passwordCheckError").innerHTML=""
+
   }
 
 
-  // 지역선택 확인
   if(area === "지역을 선택하세요."){
     document.getElementById("areaError").innerHTML="지역을 선택해주세요."
     check = false
@@ -136,7 +126,6 @@ function signUpCheck(){
     document.getElementById("areaError").innerHTML=""
   }
 
-  // 성별체크확인
   if(!gender_man && !gender_woman){
     document.getElementById("genderError").innerHTML="성별을 선택해주세요."
     check = false
@@ -152,7 +141,6 @@ function signUpCheck(){
     document.getElementById("areaError").innerHTML=""
     document.getElementById("genderError").innerHTML=""
     
-    //비동기 처리이벤트
     setTimeout(function() {
       alert("가입이 완료되었습니다.")
   },0);
